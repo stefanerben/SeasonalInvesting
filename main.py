1fa79802bad0cb672c7caab38cc4d2bb84fd9c73
Stefan <stefan.erben1@gmail.com>
2022-10-28 15:50:19 +0200

    intial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+
+
+def main():
+    print("Starting...")
+    
+    # changes holdings only two times in the year
+    # be in the consumer markets (in the S&P500 from November to March)
+    # be in the general market the rest of the time
+    # therefore positions are changing in November and March
+    
+    
+    starting_date = "Date"
+    ending_date = "Date"
+    
+    inital_portfolio_value = 100000 # 100k USD
+    
+    
+    
+    
+    
+def get_index_postioning():
+    """
+    Gets all the positions for a given index, e.g. the S&P500
+    """
+    pass
+
+def get_price_for_symbol(symbol, date="today"):
+    """
+    Given a stock symbol and an optional date, return the price of that stock
+    If no price is defined, the method returns the current price
+    """      
+    
+    
+    
+main()
