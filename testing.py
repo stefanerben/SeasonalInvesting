3648d2df43b3fbd87913ffae26865419e7312240
Stefan <stefan.erben1@gmail.com>
2022-11-08 02:17:39 +0100

    buying and selling finished, visualiziation started

--- testing.py
+++ testing.py
@@ -7,11 +7,32 @@ import json
 import datetime
 import yahoo_fin.stock_info as ys
 import yfinance as yf
-import main
+import utils
+from dateutil import relativedelta
+
 #msft = yf.Ticker("MSFT")
 #sector = main.get_stock_type("MSFT")
 #print(sector)
 
 
-price = main.get_price_for_symbol("MSFT", date=datetime.date(2022, 1, 9) )
-print(price)
+
+print(int(datetime.datetime.utcnow().timestamp()))
+
+starting_date =  datetime.date(2022, 1, 9)
+print(utils.datetime_to_int(starting_date))
+ending_date = datetime.date.today()
+current_date = starting_date
+
+utils.start_progress("Get Stock Types...")
+
+while current_date < ending_date:
+            
+    utils.progress(2 / 9 * 100)
+    #current_date = current_date + datetime.timedelta(days=1)
+    current_date = current_date + relativedelta.relativedelta(months=1, day=1)
+    print(current_date)
+
+utils.end_progress()
+
+
+
---
+++ utils.py
@@ -0,0 +1,27 @@
+import sys
+import datetime
+
+def start_progress(title):
+    global progress_x
+    sys.stdout.write(title + ": [" + "-"*40 + "]" + chr(8)*41)
+    sys.stdout.flush()
+    progress_x = 0
+
+def progress(x):
+    global progress_x
+    x = int(x * 40 // 100)
+    sys.stdout.write("#" * (x - progress_x))
+    sys.stdout.flush()
+    progress_x = x
+
+def end_progress():
+    sys.stdout.write("#" * (40 - progress_x) + "]\n")
+    sys.stdout.flush()
+
+def datetime_to_int(dateobj):
+    total = int(dateobj.strftime('%S'))
+    total += int(dateobj.strftime('%M')) * 60
+    total += int(dateobj.strftime('%H')) * 60 * 60
+    total += (int(dateobj.strftime('%j')) - 1) * 60 * 60 * 24
+    total += (int(dateobj.strftime('%Y')) - 1970) * 60 * 60 * 24 * 365
+    return total
